f00c6d2bdd1e39c53b9fff4c38af9cad2dfa55cd
Melissa Gore <mrg92@humboldt.edu>
2012-10-25 09:44:39 -0700

    adding sidebar right as float

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -32,7 +32,7 @@
 			<?php if ($tabs): ?><div id="tabs"><?php print render($tabs); ?></div><?php endif; ?>
 			<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 
-	
+			<?php print render($page['sidebar_first']); ?>
 		
 			<?php print render($page['content']); ?>
 			
